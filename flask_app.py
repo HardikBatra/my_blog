507741502eb12caa9c0507dfa394ff318986eb71
hardik <mail4hardikbatra@gmail.com>
2021-01-10 01:34:55 +0530

    databaseconnected

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/my_blog'
+db = SQLAlchemy(app)
 
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80),  nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120),  nullable=False)
+    msg = db.Column(db.String(120),  nullable=False)
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/about')
 def about():
     return render_template('about.html')
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        name=request.form.get('name')
+        email=request.form.get('email')
+        phone=request.form.get('phone')
+        msg=request.form.get('msg')
+        entry=Contacts(name=name,email=email,phone=phone,msg=msg)
+        db.session.add(entry)
+        db.session.commit()
+    
+    
+    
     return render_template('contact.html')
 @app.route('/post')
 def post():
